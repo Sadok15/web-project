575c472b5cb24fbd0e730ae11338caef9ac1e903
Sadok15 <66098460+Sadok15@users.noreply.github.com>
2021-12-08 00:36:37 +0100

    fix component architecture (#1)
    Co-authored-by: SadokFrouja <sadok.frouja@neopolis-dev.com>

--- GRH-FRONT-V1/src/app/app.module.ts
+++ GRH-FRONT-V1/src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './Acceuil/navbar/navbar.component';
 import { FooterComponent } from './Acceuil/footer/footer.component';
+import { AuthComponent } from './connexion/auth/auth.component';
+import { InscriptionComponent } from './connexion/auth_inscription/inscription.component';
+import { ListOpportuniteComponent } from './Candidat/Opportunite/list-opportunite/list-opportunite.component';
+import { MesOpportuniteComponent } from './Candidat/Opportunite/mes-opportunite/mes-opportunite.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AuthComponent,
+    InscriptionComponent,
+    ListOpportuniteComponent,
+    MesOpportuniteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # web-project
+
+installation : 
+
+npm install bootstrap
+ng add @ng-bootstrap/ng-bootstrap
